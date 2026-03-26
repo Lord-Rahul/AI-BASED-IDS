d248d33e0f59049f18269205915f549a82f9cd00
lord-rahul <rahul.23ucs087.btcs@baddiuniv.ac.in>
2026-03-26 17:19:28 +0530

    Add requirements.txt and initial train.py script with necessary imports

---
+++ ml/train.py
@@ -0,0 +1,11 @@
+import glob #for file accessing
+import os
+
+import joblib
+import numpy as np
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+
+
